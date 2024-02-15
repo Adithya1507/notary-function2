a1b8226f37bc18e02f8a43bcecd0addbee0c5e89
Adithya Ajay <141824002+Adithya-AntPhy@users.noreply.github.com>
2024-02-15 18:25:55 +0530

    sad

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ export default async ({ req, res, log, error }) => {
     // Initialize Appwrite client
     const client = new Client()
         .setEndpoint('https://cloud.appwrite.io/v1')
-        .setProject('65c9f0c6dcb3c4b99e70');
+        .setProject('65c9f0c6dcb3c4b99e70')
+        .setKey('d741f023e7ca137d07e4f11be0845db402012423a7932d481a267a1e024341286de95e80f9dd77f449d307bb665e1bd856bb6dcba0a14e123734fb0908e4348cf270540a20b78802ab2f0bcd70e574982a9562ff08fba562545a7e8383f2a30ebcf9b0e6ba82a1e53c2a12e3047efeb06d63143e256cf5a9604a183ce524a00a')
 
     try {
         // Subscribe to document update events in a specific collection
