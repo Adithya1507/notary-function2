154561ac7bd61e7099a296d714f45e1c498dee0d
Adithya Ajay <141824002+Adithya-AntPhy@users.noreply.github.com>
2024-02-16 10:49:26 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ externalClient
 //const realtime = new Realtime(externalClient)
     
 const realtime = externalClient.realtime;
-realtime.subscribe(['databases.65c9f1dab1c765f9541e.collections.65c9f1e49ee8dcddbe37.documents.test', 'files'], response => {
+externalClient.subscribe(['databases.65c9f1dab1c765f9541e.collections.65c9f1e49ee8dcddbe37.documents.test', 'files'], response => {
     // Callback will be executed on changes for documents A and all files.
     console.log(response);
 });
