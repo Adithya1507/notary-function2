d97d7672d9ec83338ad507fa9a21ebac61ee699d
Adithya Ajay <141824002+Adithya-AntPhy@users.noreply.github.com>
2024-02-16 10:46:40 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const externalClient = new Client();
 externalClient
 .setEndpoint('https://cloud.appwrite.io/v1')
 .setProject('65c9f0c6dcb3c4b99e70');
-const realtime = new Realtime(client)
+const realtime = new Realtime(externalClient)
     
 
 realtime.subscribe(['databases.65c9f1dab1c765f9541e.collections.65c9f1e49ee8dcddbe37.documents.test', 'files'], response => {
