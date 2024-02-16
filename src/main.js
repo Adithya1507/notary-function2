19f54dbf158df302427748fe58e3c16fa652d0c7
Adithya Ajay <141824002+Adithya-AntPhy@users.noreply.github.com>
2024-02-16 10:51:39 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,32 @@
+// import { Client } from "node-appwrite";
+
+
+
+// const externalClient = new Client();
+// externalClient
+// .setEndpoint('https://cloud.appwrite.io/v1')
+// .setProject('65c9f0c6dcb3c4b99e70');
+// //const realtime = new Realtime(externalClient)
+    
+// const realtime = externalClient.realtime;
+// externalClient.subscribe(['databases.65c9f1dab1c765f9541e.collections.65c9f1e49ee8dcddbe37.documents.test', 'files'], response => {
+//     // Callback will be executed on changes for documents A and all files.
+//     console.log(response);
+// });
+
+
+
 import { Client } from "node-appwrite";
 import pkg from 'node-appwrite';
 const { Realtime } = pkg;
-
-
 const externalClient = new Client();
 externalClient
-.setEndpoint('https://cloud.appwrite.io/v1')
-.setProject('65c9f0c6dcb3c4b99e70');
-//const realtime = new Realtime(externalClient)
-    
-const realtime = externalClient.realtime;
-externalClient.subscribe(['databases.65c9f1dab1c765f9541e.collections.65c9f1e49ee8dcddbe37.documents.test', 'files'], response => {
-    // Callback will be executed on changes for documents A and all files.
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('65c9f0c6dcb3c4b99e70');
+
+const realtime = new Realtime(externalClient);
+
+realtime.subscribe(['databases.65c9f1dab1c765f9541e.collections.65c9f1e49ee8dcddbe37.documents.test', 'files'], response => {
+    // Callback will be executed on changes for documents and files
     console.log(response);
 });
