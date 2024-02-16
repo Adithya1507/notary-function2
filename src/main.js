22baf038a5711b8b60db803da4ffaa67c493b290
Adithya Ajay <141824002+Adithya-AntPhy@users.noreply.github.com>
2024-02-16 10:18:38 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject('65c9f0c6dcb3c4b99e70');
 
-client.subscribe(['collections.65c9f1e49ee8dcddbe37.documents.65c9f2077e468acc87cd', 'files'], response => {
+client.subscribe(['collections.entity.documents.test', 'files'], response => {
     // Callback will be executed on changes for documents A and all files.
     console.log(response);
 });
