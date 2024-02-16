e98726625e5fb48358839a6ef65680342c31bf36
Adithya Ajay <141824002+Adithya-AntPhy@users.noreply.github.com>
2024-02-16 10:43:03 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
-import { Realtime } from "appwrite";
+import { Realtime } from "node-appwrite";
 
-const realtime = new Realtime()
-    .setEndpoint('https://cloud.appwrite.io/v1');
+
+
+const externalClient = new Client();
+externalClient
+.setEndpoint('https://cloud.appwrite.io/v1')
+.setProject('65c9f0c6dcb3c4b99e70');
+const realtime = new Realtime(client)
+    
 
 realtime.subscribe(['databases.65c9f1dab1c765f9541e.collections.65c9f1e49ee8dcddbe37.documents.test', 'files'], response => {
     // Callback will be executed on changes for documents A and all files.
